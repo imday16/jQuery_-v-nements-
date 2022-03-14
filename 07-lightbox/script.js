31f31914eafa972ae8e3232bb599c30f13fdd3c3
ayad imad <imday16@gmail.com>
2022-03-14 23:13:43 +0100

    commit light-box

---
+++ 07-lightbox/script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+    lightbox.option({
+        wrapAround : true, // mode carrousel
+        albumLabel : "Photo %1 sur %2", // %1 : numéro de la photo, %2 : nb total de photos
+        resizeDuration : 300, // defaut 700
+        fadeDuration: 1200 // defaut 600
+    })
+
+});
