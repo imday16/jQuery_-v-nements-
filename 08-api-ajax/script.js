490d49572bb4f2fc94bbc8bdd33def6d831a135a
ayad imad <imday16@gmail.com>
2022-03-14 23:15:54 +0100

    cmmint index

--- 08-api-ajax/script.js
+++ 08-api-ajax/script.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    const APIKEY = '1ff1b732'; // votre clé
+    const APIKEY = '89bc8c70'; // votre clé
     const BASEURL = `https://www.omdbapi.com/?apikey=${APIKEY}&`;
 
 
@@ -24,13 +24,14 @@ $(function () {
     });
 
     /* Lancer la recherche sur click du bouton */
-
     $('#recherche').on('click', lanceRecherche);
 
-        function lanceRecherche(event,page = 1)
+
+
+    function lanceRecherche(event, page = 1) {
         // console.log(page);
         let saisie = $('#saisie').val(); // document.querySelector('#saisie').value
-        const URLRECHERCHE = BASEURL + 's=' + saisie + '&type=movie';
+        const URLRECHERCHE = BASEURL + 's=' + saisie + '&type=movie&page=' + page;
 
         $.ajax({
             method: 'GET',
@@ -39,34 +40,70 @@ $(function () {
             success: function (reponse) {
                 $('#resultats').html('');
                 console.log(reponse);
-                if (reponse.Response.toLowerCase() == 'True') {
+                if (reponse.Response.toLowerCase() == 'true') {
 
-                    // pagination
-                    $('#pagination).html('');
-                    let nbPages = parseInt(reponse.totalResultats / 10); // ex 246/10 =24 soit 25 pages
-                    if(reponse.totalResultats % 10 > 0)nbPages++; //246%10 =>6 24+1=25
+                    // Pagination
+                    $('#pagination').html('');
+                    let nbPages = parseInt(reponse.totalResults / 10); // ex 246/10 => 24
+                    if (reponse.totalResults % 10 > 0) nbPages++; // 246%10 => 6  24+1 = 25 pages
                     console.log(nbPages);
-                    if(nbPages > 1){
-                        for(let i=1; i<=nbPages;i++){
+                    if (nbPages > 1) {
+                        for (let i = 1; i <= nbPages; i++) {
                             let bouton = document.createElement('button');
                             $(bouton).attr({
-                                'data-page' : i,
-                                'class' : 'bouton-pagination'
+                                'data-page': i,
+                                'class': 'bouton-pagination btn btn-sm'
                             });
-                            if(i==page) $(bouton).addClass('active');
+
+                            if (i == page) $(bouton).addClass('active');
                             $(bouton).html(i);
                             $('#pagination').append(bouton);
                         }
-                    }                                                                             
 
-                    //Affichage des résultats
+                    }
+
+                    // clic sur un bouton de pagination
+                    $(".bouton-pagination").on('click', function (event) {
+                        let pageCible = $(this).attr('data-page');
+                        // console.log("je veux la page "+pageCible);
+                        lanceRecherche(event, pageCible);
+                    });
+
+                    // Affichage des résultats
+                    /* 
+                    for(let i = 0; i< Search.length; i++){
+                        let film = Search[i];
+                        let figure = document.createElement('figure');
+                        let img = document.createElement('img');
+                        let caption = document.createElement('figcaption');
+                        // img.setAttribute('src',film.Poster);
+                        // $(img).attr('src',film.Poster);
+                        // $(img).attr('alt',film.Title);
+                        $(img).attr({
+                            'src' : film.Poster,
+                            'alt': film.Title
+                        });
+                        // figure.appendChild(img);
+                        $(figure).append(img);
+                        $(caption).html(`${film.Title} (${film.Year})`);
+                        $(figure).append(caption);
+                        $('#resultats').append(figure);
+                    }
+                    */
+
                     reponse.Search.forEach(
+                        // function(film){
                         (film) => {
                             let figure = document.createElement('figure');
                             let img = document.createElement('img');
                             let caption = document.createElement('figcaption');
                             // img.setAttribute('src',film.Poster);
-                            $(img).attr('src',film.Poster);
+                            // $(img).attr('src',film.Poster);
+                            // $(img).attr('alt',film.Title);
+                            $(img).attr({
+                                'src': film.Poster,
+                                'alt': film.Title
+                            });
                             // figure.appendChild(img);
                             $(figure).append(img);
                             $(caption).html(`${film.Title} (${film.Year})`);
@@ -79,19 +116,33 @@ $(function () {
                 }
             },
             beforeSend: function () {
-                $('#resultats').html('Recherche en cours...');
+                $('#resultats').html('<img src="dark-loader.gif" class="loader">');
             }
         });
 
+        // $.get()
+        // $.get(URLRECHERCHE, {}, function(reponse){}, 'json');
+        /*
+            $.ajax({
+                method :'GET',
+                url : URLRECHERCHE,
+                data: {},
+                success : function(reponse){},
+                dataType : 'json'    
+            });
 
-    });
+        */
+
+        // $.post()
+        // $.post(URLRECHERCHE, {}, function(reponse){}, 'json');
+    }
+
+
+    $.get('copyright.txt', {}, function (reponse) {
+        $('footer .row .col').html(reponse);
+    }, 'text');
 
 
 
 
-                                      
-
-
-
-});                                                                        
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+}); // Fin du DOM chargé
